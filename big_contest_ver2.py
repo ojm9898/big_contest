e73595d928f6aac28e642b64499efbe63dddba11
ojm9898 <ojm9898@hanyang.ac.kr>
2016-09-06 15:44:38 +0900

    ver 2.0

--- big_contest_ver2.py
+++ big_contest_ver2.py
@@ -10,13 +10,13 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-file_list = glob.glob('*.xlsx')
-worksheet_list = []
-
-for f in file_list :
-    workbook = xlrd.open_workbook(f)
-    temp = {'worksheet' : workbook.sheet_by_index(0), 'nrows' : workbook.sheet_by_index(0).nrows }
-    worksheet_list.append(temp)
+def readFile(file_list) :
+    worksheet_list = []
+    for f in file_list :
+        workbook = xlrd.open_workbook(f)
+        temp = {'worksheet' : workbook.sheet_by_index(0), 'nrows' : workbook.sheet_by_index(0).nrows }
+        worksheet_list.append(temp)
+    return worksheet_list
 
 def isNumber(s) :
     try :
@@ -25,34 +25,43 @@ def isNumber(s) :
     except ValueError :
         return False
 
-row_val = []
-frame_list = []
-date = ""
-for worksheet in worksheet_list :
-    for row_num in range(worksheet['nrows']) :
-        if str(worksheet['worksheet'].row_values(row_num)[0]).find("년") != -1 :
-            date = worksheet['worksheet'].row_values(row_num)[0]
-        elif isNumber(worksheet['worksheet'].row_values(row_num)[0]) :
-            row_val.append(worksheet['worksheet'].row_values(row_num))
-        elif str(worksheet['worksheet'].row_values(row_num)[0]).find("합계") != -1 :
-            frame = DataFrame(row_val, columns = ['rank','name','start_date','sales_account','sales_account_ratio','sales_account_varience','sales_account_varience_ratio','accum_sales_account','audience','audience_varience','audience_varience_ratio','accum_audience', 'screen','show','main_country','country','maker','distributor','rating','genre','director','actor'])
-            del frame['sales_account_ratio']
-            del frame['sales_account_varience']
-            del frame['sales_account_varience_ratio']
-            del frame['accum_sales_account']
-            del frame['screen']
-            frame['date'] = date
-            frame.fillna(0)
-            frame_list.append(frame)
-            row_val = []
-            date = ""
+def makeDataFrame(worksheet_list) :
+    row_val = []
+    frame_list = []
+    date = ""
+    for worksheet in worksheet_list :
+        for row_num in range(worksheet['nrows']) :
+            if str(worksheet['worksheet'].row_values(row_num)[0]).find("년") != -1 :
+                date = worksheet['worksheet'].row_values(row_num)[0]
+            elif isNumber(worksheet['worksheet'].row_values(row_num)[0]) :
+                row_val.append(worksheet['worksheet'].row_values(row_num))
+            elif str(worksheet['worksheet'].row_values(row_num)[0]).find("합계") != -1 :
+                frame = DataFrame(row_val, columns = ['rank','name','start_date','sales_account','sales_account_ratio','sales_account_varience','sales_account_varience_ratio','accum_sales_account','audience','audience_varience','audience_varience_ratio','accum_audience', 'screen','show','main_country','country','maker','distributor','rating','genre','director','actor'])
+                del frame['sales_account_ratio']
+                del frame['sales_account_varience']
+                del frame['sales_account_varience_ratio']
+                del frame['accum_sales_account']
+                del frame['screen']
+                frame['date'] = date
+                frame.fillna(0)
+                frame_list.append(frame)
+                row_val = []
+                date = ""
+    return frame_list
 
-f = open("frame_data.txt",'w')
-f.close()
+def writeDataFrame(frame_list) :
+    f = open("frame_data.txt",'w')
+    f.close()
+    for frame in frame_list :
+        frame.to_csv("frame_data.txt", sep = '\t', mode = 'a', encoding = 'utf-8', index = False)
+        with open("frame_data.txt",'a') as f:
+            f.write("\n")
+    print "success"
 
-for frame in frame_list :
-    frame.to_csv("frame_data.txt", sep = '\t', mode = 'a', encoding = 'utf-8', index = False)
-    with open("frame_data.txt",'a') as f:
-        f.write("\n")
+file_list = glob.glob('*.xlsx')
 
-print "success"
+worksheet_list = readFile(file_list)
+
+frame_list = makeDataFrame(worksheet_list)
+
+print "All has been done!"
